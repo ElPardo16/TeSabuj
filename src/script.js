72c865f4cce6f05196ab962c262d6ae714c2c8b7
ElPardo16 <andresito105@gmail.com>
2022-10-21 14:57:29 -0500

    mejoras

--- src/script.js
+++ src/script.js
@@ -1,9 +1,16 @@
-function ProductPage(obj){
+function saveData(obj){
     var tittle = obj.childNodes[3].innerText
     var imgSrc = obj.childNodes[1].src
     srcs = imgSrc.split("/src")
     localStorage.setItem("tittle", tittle)
     localStorage.setItem("src", srcs[1])
-    
+}
+function ProductPage(obj){
+    saveData(obj)
     window.open("./src/product.html", "_self")
+}
+function ProductPage2(obj){
+    saveData(obj)
+    location.reload()
+    window.scrollTo(0,0)
 }
